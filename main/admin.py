5d04ae09e2fd2e817c8a2d2cdcca4b3fc42ad72b
Akimenko_Ivan <412871@niuitmo.ru>
2024-03-08 18:58:37 +0300

    + filter, search, represent

--- main/admin.py
+++ main/admin.py
@@ -5,9 +5,11 @@ from main.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('id', 'name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price')
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
